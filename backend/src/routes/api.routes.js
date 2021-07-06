c18d29048ed1b31951d2d60b2b5f7db2963409ad
bagotaj <bagotaj@gmail.com>
2021-07-06 17:57:53 +0200

    Adding /login route

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { registrationController } from '../controllers/registrationController';
+import { loginController } from '../controllers/loginController';
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.use(cors());
 router.use(express.json());
 
 router.post('/register', registrationController.post);
+router.post('/login', loginController.post);
 
 export default router;
