3d9fae1d4d677829885f1036a1112c4b5abc2b3b
bagotaj <bagotaj@gmail.com>
2021-07-06 17:58:50 +0200

    Adding login backend files

---
+++ backend/src/controllers/loginController.js
@@ -0,0 +1,8 @@
+import { loginService } from '../services/loginService';
+
+export const loginController = {
+  async post(req, res) {
+    const data = await loginService.loginUser(req.body);
+    return res.status(data.status).json(data);
+  },
+};
---
+++ backend/src/services/loginService.js
@@ -0,0 +1,45 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import { loginValidation } from '../loginValidation';
+import User from '../models/User';
+
+export const loginService = {
+  async loginUser(loginData) {
+    const { error } = loginValidation(loginData);
+    if (error) {
+      return {
+        status: 400,
+        message: error.details[0].message,
+      };
+    }
+
+    const user = await User.findOne({ email: loginData.email });
+    if (!user) {
+      return {
+        status: 400,
+        message: 'Email is not registered',
+      };
+    }
+
+    const validPass = await bcrypt.compare(loginData.password, user.password);
+    if (!validPass) {
+      return {
+        status: 400,
+        message: 'Username or password is incorrect',
+      };
+    }
+
+    const authToken = jwt.sign(
+      { firstName: user.firstName, role: user.role },
+      process.env.TOKEN_SECRET
+    );
+    return {
+      status: 200,
+      message: 'Logged in!',
+      token: authToken,
+      firstName: user.firstName,
+      role: user.role,
+    };
+  },
+};
