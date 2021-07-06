93d7583fba52c493ca168e1c2a21b7a402303c94
bagotaj <bagotaj@gmail.com>
2021-07-06 17:52:19 +0200

    Adding routes to App.js / Moving Router to index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,53 @@
+import React, { useState } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+
 import './App.scss';
 
-function App() {
-  return <div className="App">klmgk</div>;
-}
+import Navbar from './components/common/Navbar/Navbar';
+import Login from './components/pages/Login/Login';
+import Register from './components/pages/Register/Register';
+import Main from './components/pages/Main/Main';
+
+const App = () => {
+  const history = useHistory();
+
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  function handleLocalStorage(user) {
+    return localStorage.setItem('loggedInUser', JSON.stringify(user));
+  }
+
+  const handleLogout = async e => {
+    e.preventDefault();
+
+    localStorage.clear();
+    setLoggedInUser(null);
+
+    history.push('/login');
+  };
+
+  return (
+    <div className="App">
+      <div className="cotainer">
+        <Navbar user={loggedInUser} handleLogout={handleLogout} />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route
+            exact
+            path="/login"
+            component={() => (
+              <Login
+                handleLoggedInUser={setLoggedInUser}
+                handleLocalStorage={handleLocalStorage}
+                loggedInUser={loggedInUser}
+              />
+            )}
+          />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </div>
+    </div>
+  );
+};
 
 export default App;
