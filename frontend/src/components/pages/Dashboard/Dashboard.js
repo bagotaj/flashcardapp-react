d663698e3222e3a95613516f2a4385beb497e7dd
bagotaj <bagotaj@gmail.com>
2021-07-08 19:34:16 +0200

    Dashboard blank page

--- frontend/src/components/pages/Dashboard/Dashboard.js
+++ frontend/src/components/pages/Dashboard/Dashboard.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>khgmklg</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col col-lg-2" />
+        <div className="col-md-auto">Próbáld ki!</div>
+        <div className="col col-lg-2" />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
