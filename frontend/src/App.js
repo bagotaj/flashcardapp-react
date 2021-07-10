9da59d6d5ac415c0c8b108a22366591f4f81fe37
bagotaj <bagotaj@gmail.com>
2021-07-10 17:02:58 +0200

    Adding app-main class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Login from './components/pages/Login/Login';
 import Register from './components/pages/Register/Register';
 import Main from './components/pages/Main/Main';
 import Dashboard from './components/pages/Dashboard/Dashboard';
+import NewCards from './components/pages/NewCards/NewCards';
 
 const App = () => {
   const history = useHistory();
@@ -31,22 +32,25 @@ const App = () => {
     <div className="App">
       <div className="container">
         <Navbar user={loggedInUser} handleLogout={handleLogout} />
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route
-            exact
-            path="/login"
-            component={() => (
-              <Login
-                handleLoggedInUser={setLoggedInUser}
-                handleLocalStorage={handleLocalStorage}
-                loggedInUser={loggedInUser}
-              />
-            )}
-          />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/dashboard" component={Dashboard} />
-        </Switch>
+        <div className="app-main">
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route
+              exact
+              path="/login"
+              component={() => (
+                <Login
+                  handleLoggedInUser={setLoggedInUser}
+                  handleLocalStorage={handleLocalStorage}
+                  loggedInUser={loggedInUser}
+                />
+              )}
+            />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/cards/new" component={NewCards} />
+          </Switch>
+        </div>
       </div>
     </div>
   );
