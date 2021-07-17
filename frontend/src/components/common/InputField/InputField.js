ca330d29d715a8e301d4b6f28673eeb69dbf84d5
bagotaj <bagotaj@gmail.com>
2021-07-17 20:31:07 +0200

    Adding select to component / Changing onChange and onBlure props

--- frontend/src/components/common/InputField/InputField.js
+++ frontend/src/components/common/InputField/InputField.js
@@ -6,43 +6,72 @@ const InputField = props => {
     type,
     name,
     value,
-    onChange,
-    onBlur,
+    handleInputChange,
+    handleInputBlur,
     reference,
     error,
     labelText,
     required,
+    options,
   } = props;
+
+  let inputField;
+
+  if (type === 'select') {
+    inputField = (
+      <select
+        id={name}
+        name={name}
+        required={required}
+        className="form-select"
+        onChange={handleInputChange}
+        onBlur={handleInputBlur}
+        ref={reference}
+        value={value}
+      >
+        {options.map(option => (
+          <option value={option.value} key={option.text}>
+            {option.text ? option.text : option.value}
+          </option>
+        ))}
+      </select>
+    );
+  } else if (type === 'textarea') {
+    inputField = (
+      <textarea
+        className="form-control"
+        id={name}
+        name={name}
+        onChange={handleInputChange}
+        onBlur={handleInputBlur}
+        required={required}
+        ref={reference}
+        rows="3"
+        value={value}
+      />
+    );
+  } else {
+    inputField = (
+      <input
+        className="form-control"
+        id={name}
+        type={type}
+        name={name}
+        value={value}
+        onChange={handleInputChange}
+        onBlur={handleInputBlur}
+        required={required}
+        ref={reference}
+      />
+    );
+  }
+
   return (
     <div className={`${error && 'was-validated'} text-start`}>
       <label className="form-label mt-3" htmlFor={name}>
         {labelText}
       </label>
-      {type === 'textarea' ? (
-        <textarea
-          className="form-control"
-          id={name}
-          name={name}
-          onChange={onChange}
-          onBlur={onBlur}
-          required={required}
-          ref={reference}
-          rows="3"
-          value={value}
-        />
-      ) : (
-        <input
-          className="form-control"
-          id={name}
-          type={type}
-          name={name}
-          value={value}
-          onChange={onChange}
-          onBlur={onBlur}
-          required={required}
-          ref={reference}
-        />
-      )}
+      {inputField}
       <div className="invalid-feedback mx-2">{error}</div>
     </div>
   );
