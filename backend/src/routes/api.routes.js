47807f04ee173718543bcffa765ac5ea9c86f57e
bagotaj <bagotaj@gmail.com>
2021-07-20 20:39:13 +0200

    Adding othercards api route

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -5,6 +5,7 @@ import { registrationController } from '../controllers/registrationController';
 import { loginController } from '../controllers/loginController';
 import { usersController } from '../controllers/usersController';
 import { languageCardsController } from '../controllers/languageCardsController';
+import { otherCardsController } from '../controllers/otherCardsController';
 import authenticateToken from '../middlewares/authenticateToken';
 
 const router = express.Router();
@@ -18,6 +19,6 @@ router.get('/users', authenticateToken, usersController.get);
 // router.get('/languagecards', languageCardsController.get);
 router.post('/languagecards/new', languageCardsController.post);
 // router.get('/othercards', otherCardsController.get);
-// router.post('/othercards/new', otherCardsController.post);
+router.post('/othercards/new', otherCardsController.post);
 
 export default router;
