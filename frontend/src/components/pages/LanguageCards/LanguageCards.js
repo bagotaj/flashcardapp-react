11222df956fa8e5c139125bfe9115e95fdf8f8d1
bagotaj <bagotaj@gmail.com>
2021-07-24 16:16:50 +0200

    Making basic language cards component

---
+++ frontend/src/components/pages/LanguageCards/LanguageCards.js
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv';
+import React, { useEffect, useState } from 'react';
+
+dotenv.config();
+
+const LanguageCards = props => {
+  const { token } = props;
+  const server = process.env.REACT_APP_SERVER_URL;
+
+  const [cards, setCards] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${server}/api/languagecards`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(res => {
+        if (res.status !== 200) {
+          throw Error(`Nem sikerült lekérni az adotokat. ${res.status}`);
+        }
+        return res.json();
+      })
+      .then(jsonRes => {
+        setCards(jsonRes);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
+      });
+  }, []);
+
+  return (
+    <main className="userList container">
+      <h2>Szókártyák</h2>
+
+      <div>{error && <div className="error">{error}</div>}</div>
+      {cards.map(card => (
+        <div className="box center-content" key={card._id}>
+          {card.cardTitle}
+        </div>
+      ))}
+    </main>
+  );
+};
+
+export default LanguageCards;
