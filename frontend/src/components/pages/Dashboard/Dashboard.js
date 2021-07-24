7f0f5a20670896a4bf96dec70c4f9660baf7d336
bagotaj <bagotaj@gmail.com>
2021-07-24 15:43:11 +0200

    Adding two types of cards link

--- frontend/src/components/pages/Dashboard/Dashboard.js
+++ frontend/src/components/pages/Dashboard/Dashboard.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Dashboard.scss';
 
 const Dashboard = () => {
+  // const { loggedInUser } = props;
+
+  // const [userCards, setUserCards] = useState([]);
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col col-lg-2" />
+    <div className="container center-content">
+      <div className="row mt-3">
         <div className="col-md-auto d-flex">
-          <div className="box">Kártyacsomag 1</div>
-          <div className="box">Kártyacsomag 2</div>
+          <div className="box center-content">
+            <Link to="/languagecards">Szókártyák</Link>
+          </div>
+          <div className="box center-content">
+            <Link to="/othercards">Egyéb kártyák</Link>
+          </div>
         </div>
-        <div className="col col-lg-2" />
       </div>
     </div>
   );
