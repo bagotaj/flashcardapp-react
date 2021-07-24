934d70f318265930eaba6c3816bdafaa6659fbea
bagotaj <bagotaj@gmail.com>
2021-07-24 15:23:00 +0200

    Rewriting code

--- backend/src/middlewares/authenticateToken.js
+++ backend/src/middlewares/authenticateToken.js
@@ -1,19 +1,24 @@
+import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+
+dotenv.config();
 
 // eslint-disable-next-line consistent-return
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  const authHeader = req.header('Authorization');
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (token === null) return res.sendStatus(401);
+  if (!token) return res.sendStatus(401).send('Hozzáférés megtagadva');
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+  try {
+    const verifiedUser = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    req.user = user;
+    req.user = verifiedUser;
 
     next();
-  });
+  } catch (error) {
+    res.status(401).send('Érvénytelen token');
+  }
 };
 
 export default authenticateToken;
