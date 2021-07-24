9b6822f57530bb281d3648202ec5d73d7e5f45cf
bagotaj <bagotaj@gmail.com>
2021-07-24 15:16:37 +0200

    Adding admin role to find data part

--- backend/src/controllers/languageCardsController.js
+++ backend/src/controllers/languageCardsController.js
@@ -1,6 +1,22 @@
+import { LanguageCard } from '../models/Cards';
 import { languageCardsService } from '../services/languageCardsService';
 
 export const languageCardsController = {
+  async get(req, res, next) {
+    try {
+      await LanguageCard.find()
+        .then(foundCards => {
+          if (req.user.role === 'admin') {
+            return foundCards;
+          }
+          return foundCards.filter(card => card.userId === req.user.userId);
+        })
+        .then(foundCards => res.status(200).json(foundCards));
+    } catch (err) {
+      next(err);
+    }
+  },
+
   async post(req, res) {
     const data = await languageCardsService.saveCards(req.body);
     res.status(data.status).json(data);
