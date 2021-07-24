50293509f14e3efb312d0476ce69fe9f3d4aefd2
bagotaj <bagotaj@gmail.com>
2021-07-24 21:08:38 +0200

    Making OtherCards component to list cards

---
+++ frontend/src/components/pages/OtherCards/OtherCards.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+const OtherCards = props => {
+  const { token } = props;
+
+  const server = process.env.REACT_APP_SERVER_URL;
+
+  const [cards, setCards] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${server}/api/othercards`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(res => {
+        if (res.status !== 200) {
+          throw Error(`Nem sikerült lekérni az adatokat. ${res.status}`);
+        }
+        return res.json();
+      })
+      .then(jsonRes => {
+        setCards(jsonRes);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
+      });
+  }, []);
+
+  return (
+    <main className="userList container">
+      <h2>Egyéb kártyák</h2>
+
+      <div>{error && <div className="error">{error}</div>}</div>
+      {cards.map(card => (
+        <div className="box center-content" key={card._id}>
+          {card.cardTitle}
+        </div>
+      ))}
+    </main>
+  );
+};
+
+export default OtherCards;
