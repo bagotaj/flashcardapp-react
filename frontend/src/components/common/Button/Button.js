b9db59c897dcf4fb546ef596c4674befe4cec8dc
bagotaj <bagotaj@gmail.com>
2021-07-25 19:56:30 +0200

    Adding Link router

--- frontend/src/components/common/Button/Button.js
+++ frontend/src/components/common/Button/Button.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Button = ({ buttonType, classes, title, onClick, dataid }) => {
+const Button = ({
+  buttonType,
+  classes,
+  title,
+  onClick,
+  dataid,
+  linkRouterPath,
+}) => {
+  if (linkRouterPath) {
+    return (
+      <Link to={linkRouterPath} className={classes} data-id={dataid}>
+        {title}
+      </Link>
+    );
+  }
+
   return (
     <button
       type={buttonType ? 'submit' : 'button'}
