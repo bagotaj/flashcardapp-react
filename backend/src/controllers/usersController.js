5aca2f0d059ca62eb0786beb389ba6ca881cc7b0
bagotaj <bagotaj@gmail.com>
2021-07-27 14:24:54 +0200

    Adding getId and put methods

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -1,4 +1,5 @@
 import User from '../models/User';
+import { userService } from '../services/userService';
 
 export const usersController = {
   get(req, res) {
@@ -7,5 +8,22 @@ export const usersController = {
     } catch (err) {
       res.status(400).send(err);
     }
+  },
+
+  async getId(req, res) {
+    try {
+      const user = await User.findById(req.params.id);
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(404).json(err);
+    }
+  },
+
+  async put(req, res) {
+    const { id } = req.params;
+    const reqData = req.body;
+
+    const data = await userService.updateUser(id, reqData);
+    res.status(data.status).json(data);
   },
 };
