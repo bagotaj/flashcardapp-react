c544fdccaf0482d3bc2e9c1fa2b80ec31b4f2594
bagotaj <bagotaj@gmail.com>
2021-07-27 14:14:16 +0200

    Deleting unused code part

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 
-import { api, system } from './routes';
+import { api } from './routes';
 import logger from './logger';
 import errorHandler from './middlewares/error-handler';
 
@@ -10,7 +10,6 @@ const app = express();
 app.use(morgan('combined', { stream: logger.stream }));
 
 app.use('/api', api);
-app.use('/system', system);
 
 app.use(errorHandler);
 
