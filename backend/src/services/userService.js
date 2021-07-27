e6d4aa2bae0133b9a3ad2cafeb69341583f07ee1
bagotaj <bagotaj@gmail.com>
2021-07-27 14:23:09 +0200

    Making userService with updateUser function

---
+++ backend/src/services/userService.js
@@ -0,0 +1,31 @@
+import logger from '../logger';
+import User from '../models/User';
+import { userValidation } from '../validators/userValidation';
+
+export const userService = {
+  async updateUser(id, reqData) {
+    const { _id, __v, updatedAt, ...others } = reqData;
+    const { error } = userValidation(others);
+
+    if (error) {
+      return {
+        status: 400,
+        message: error.details[0].message,
+      };
+    }
+
+    try {
+      await User.findByIdAndUpdate(id, reqData, { useFindAndModify: false });
+      return {
+        status: 200,
+        message: 'Felhasználói adatok frissítve!',
+      };
+    } catch (err) {
+      logger.error(err);
+      return {
+        status: 500,
+        message: 'Valami nem működik',
+      };
+    }
+  },
+};
