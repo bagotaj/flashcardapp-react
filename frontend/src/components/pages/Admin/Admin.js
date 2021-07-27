7c4e6f1c8fc383debdae5955fbdc6ddb3914a567
bagotaj <bagotaj@gmail.com>
2021-07-27 14:35:36 +0200

    Making Admin component

---
+++ frontend/src/components/pages/Admin/Admin.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Admin = () => {
+  return (
+    <div>
+      <Link to="/users" className="box center-content">
+        Felhasználók listája
+      </Link>
+      <Link to="/languagecards" className="box center-content">
+        Szókártya lista
+      </Link>
+      <Link to="/othercards" className="box center-content">
+        Egyéb kártyák listája
+      </Link>
+    </div>
+  );
+};
+
+export default Admin;
