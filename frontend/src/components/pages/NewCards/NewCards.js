ce472b795ad90415fa89068b7898ea3bf8463bd1
bagotaj <bagotaj@gmail.com>
2021-07-28 15:38:59 +0200

    Adding Title to the form

--- frontend/src/components/pages/NewCards/NewCards.js
+++ frontend/src/components/pages/NewCards/NewCards.js
@@ -3,7 +3,12 @@ import FormCards from '../../common/FormCards/FormCards';
 const NewCards = props => {
   const { loggedInUser } = props;
 
-  return <FormCards type="new" loggedInUser={loggedInUser} />;
+  return (
+    <div className="row mt-5">
+      <h2>Kártyacsomag létrehozása</h2>
+      <FormCards type="new" loggedInUser={loggedInUser} />;
+    </div>
+  );
 };
 
 export default NewCards;
