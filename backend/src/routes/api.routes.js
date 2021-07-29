1759ac23c5a93a1f862c543f417bab89c60e31d3
bagotaj <bagotaj@gmail.com>
2021-07-29 14:59:01 +0200

    Adding get and put methods

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -16,13 +16,27 @@ router.use(express.json());
 router.post('/register', registrationController.post);
 router.post('/login', loginController.post);
 router.get('/users', authenticateToken, usersController.get);
+router.get('/users/:id', authenticateToken, usersController.getId);
+router.put('/users/:id', authenticateToken, usersController.put);
 router.get('/languagecards', authenticateToken, languageCardsController.get);
 router.post(
   '/languagecards/new',
   authenticateToken,
   languageCardsController.post
 );
+router.get(
+  '/languagecards/:id',
+  authenticateToken,
+  languageCardsController.getId
+);
+router.put(
+  '/languagecards/:id',
+  authenticateToken,
+  languageCardsController.put
+);
 router.get('/othercards', authenticateToken, otherCardsController.get);
 router.post('/othercards/new', authenticateToken, otherCardsController.post);
+router.get('/othercards/:id', authenticateToken, otherCardsController.getId);
+router.put('/othercards/:id', authenticateToken, otherCardsController.put);
 
 export default router;
