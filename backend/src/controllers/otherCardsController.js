f6d5217773c25789ad8afa577856112740e682cf
bagotaj <bagotaj@gmail.com>
2021-07-29 14:58:05 +0200

    Adding getId and put methods

--- backend/src/controllers/otherCardsController.js
+++ backend/src/controllers/otherCardsController.js
@@ -17,8 +17,25 @@ export const otherCardsController = {
     }
   },
 
+  async getId(req, res) {
+    try {
+      const card = await OtherCard.findById(req.params.id);
+      return res.status(200).json(card);
+    } catch (err) {
+      return res.status(404).json(err);
+    }
+  },
+
   async post(req, res) {
     const data = await otherCardsService.saveCards(req.body);
+    res.status(data.status).json(data);
+  },
+
+  async put(req, res) {
+    const { id } = req.params;
+    const reqData = req.body;
+
+    const data = await otherCardsService.updateCards(id, reqData);
     res.status(data.status).json(data);
   },
 };
