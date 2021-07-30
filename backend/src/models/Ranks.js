fd76f3768ab361420bdd9e34af9a95f63e9e1767
bagotaj <bagotaj@gmail.com>
2021-07-30 19:48:41 +0200

    Making Ranks schema model

---
+++ backend/src/models/Ranks.js
@@ -0,0 +1,23 @@
+import { model, Schema } from 'mongoose';
+
+const rankSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 1,
+  },
+  userName: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 1,
+  },
+  points: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
+
+export const Ranks = model('Ranks', rankSchema, 'ranks');
