f1a1eb431a096cb9df6f62a42db0741a3d2a5e71
bagotaj <bagotaj@gmail.com>
2021-07-30 19:50:12 +0200

    Making rankValidation

---
+++ backend/src/validators/rankValidation.js
@@ -0,0 +1,12 @@
+const Joi = require('@hapi/joi');
+
+const rankValidation = data => {
+  const schema = Joi.object({
+    userId: Joi.string().min(1).required(),
+    userName: Joi.string().min(1).required(),
+    points: Joi.number().min(1),
+  });
+  return schema.validate(data);
+};
+
+module.exports.rankValidation = rankValidation;
