2d71d14035179bc339cd9a95198f41e928a7a905
bagotaj <bagotaj@gmail.com>
2021-07-30 19:40:42 +0200

    Adding rank point

--- frontend/src/components/pages/Cards/Cards.js
+++ frontend/src/components/pages/Cards/Cards.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Prompt } from 'react-router-dom';
 
 import './Cards.scss';
 
 const Cards = () => {
   const location = useLocation();
-  const { card } = location.state;
-  console.log(card);
+  const { card, loggedInUser } = location.state;
+  const rankId = '61041245e1afc9df8aab4171';
 
   const [cardPack, setCardPack] = useState({});
   const [flashcards, setFlashcards] = useState([]);
   const [counter, setCounter] = useState(0);
-  // const [rankPoints, setRankPoints] = useState(0);
-  console.log(cardPack);
-  console.log(flashcards);
+  const [rankPoints, setRankPoints] = useState(0);
+  const [shouldBlockNavigation] = useState(true);
+  const [alert, setAlert] = useState(null);
+  console.log(rankPoints);
+  console.log(loggedInUser);
 
   useEffect(() => {
     const setUpFormData = {
@@ -27,17 +29,74 @@ const Cards = () => {
   }, [card]);
 
   const handlerOnClickIncreaseCounter = () => {
-    const value = counter + 1;
+    let value = counter + 1;
+    let rankValue = rankPoints + 1;
+
+    if (counter === flashcards.length) {
+      value = flashcards.length;
+      rankValue = rankPoints;
+    }
+
     setCounter(value);
+    setRankPoints(rankValue);
   };
 
   const handlerOnClickDecreaseCounter = () => {
-    const value = counter - 1;
+    let value = counter - 1;
+    if (counter === -1) {
+      value = -1;
+    }
     setCounter(value);
+  };
+
+  const server = process.env.REACT_APP_SERVER_URL;
+
+  const setSentData = () => {
+    const sentData = {
+      userName: loggedInUser.firstName,
+      userId: loggedInUser.userId,
+      points: rankPoints,
+    };
+
+    return sentData;
+  };
+
+  const uploadRankData = () => {
+    console.log(setSentData());
+    fetch(`${server}/api/ranks/${rankId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${loggedInUser.token}`,
+      },
+      body: JSON.stringify(setSentData()),
+    })
+      .then(res => {
+        if (res.status !== 200) {
+          throw Error(`Nem sikerült menteni az adatokat. ${res.status}`);
+        }
+        setAlert(null);
+      })
+      .catch(err => {
+        setAlert({ alertType: 'danger', message: err.message });
+      });
   };
 
   return (
     <>
+      {alert && (
+        <div>
+          <p className={`alert alert-${alert.alertType}`}>{alert.message}</p>
+        </div>
+      )}
+      <Prompt
+        when={shouldBlockNavigation}
+        message={() => {
+          uploadRankData();
+
+          return `${rankPoints} pontot értél el!`;
+        }}
+      />
       <div className="mt-5">
         <h2>{cardPack.cardTitle}</h2>
         <button
@@ -47,24 +106,23 @@ const Cards = () => {
         >
           {'<'}
         </button>
-        {flashcards && (
-          <div className="box">
-            <div className="flip-card">
-              <div className="flip-card-inner">
-                <div className="flip-card-front center-content">
-                  {counter >= flashcards.length || counter < 0
-                    ? 'Nincs több kártya'
-                    : flashcards[counter].side1}
-                </div>
-                <div className="flip-card-back center-content">
-                  {counter >= flashcards.length || counter < 0
-                    ? 'Nincs több kártya'
-                    : flashcards[counter].side2}
+        {flashcards &&
+          (counter >= flashcards.length || counter < 0 ? (
+            <div className="box center-content">Nincs több kártya</div>
+          ) : (
+            <div className="box">
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front center-content">
+                    {flashcards[counter].side1}
+                  </div>
+                  <div className="flip-card-back center-content">
+                    {flashcards[counter].side2}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
+          ))}
         <button
           type="button"
           className="no-button"
