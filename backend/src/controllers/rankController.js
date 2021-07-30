7ae7260d7625ebc3be2f60119bcbc44792b35307
bagotaj <bagotaj@gmail.com>
2021-07-30 19:47:47 +0200

    Making rankController

---
+++ backend/src/controllers/rankController.js
@@ -0,0 +1,20 @@
+import { rankService } from '../services/rankService';
+import { Ranks } from '../models/Ranks';
+
+export const rankController = {
+  get(req, res) {
+    try {
+      Ranks.find().then(foundRanks => res.status(200).json(foundRanks));
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  },
+
+  async put(req, res) {
+    const { id } = req.params;
+    const reqData = req.body;
+
+    const data = await rankService.updateRanks(id, reqData);
+    res.status(data.status).json(data);
+  },
+};
