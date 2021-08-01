4282eba1f18e8971ec7c1b761b5e1fc047a0541c
bagotaj <bagotaj@gmail.com>
2021-08-01 19:10:31 +0200

    Creating PrivateRoute component

---
+++ frontend/src/hoc/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import NotFound from '../../components/pages/NotFound/NotFound';
+
+const PrivateRoute = ({ component: Component, path }) => {
+  const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+  console.log(loggedInUser);
+
+  if (!loggedInUser) {
+    return <Redirect to="/login" />;
+  }
+
+  if (loggedInUser.role === 'user') {
+    if (path === '/admin' || path === '/users') {
+      return <NotFound />;
+    }
+    return (
+      <Route exact path={path}>
+        <Component loggedInUser={loggedInUser} />
+      </Route>
+    );
+  }
+
+  return (
+    <Route exact path={path}>
+      <Component loggedInUser={loggedInUser} />
+    </Route>
+  );
+};
+
+export default PrivateRoute;
