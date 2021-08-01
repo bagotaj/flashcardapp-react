c7d1ff83901c48be88c7d038d4f27235c9af2180
bagotaj <bagotaj@gmail.com>
2021-08-01 19:09:47 +0200

    Creating NotFound component

---
+++ frontend/src/components/pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="mt-5">
+      <h2>404</h2>
+      <h2>Page Not Found</h2>
+      <h2>Az oldal nem található</h2>
+    </div>
+  );
+};
+
+export default NotFound;
